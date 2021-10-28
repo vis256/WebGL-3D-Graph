2246e92bc92e686af7a8d35b6531c80584c039ad
nebulazc <67900555+nebulazc@users.noreply.github.com>
2021-10-28 13:35:12 +0200

    added basic node spread

--- real.js
+++ real.js
@@ -22,7 +22,17 @@ function init() {
     // x.connectedTo.push(y);
     graphNodes.push( new node(5, "#ffab33", new THREE.Vector3(9,0,0), []) );
     graphNodes.push( new node(15, "#0455cd", new THREE.Vector3(0,0,0), []) );
-    graphNodes.push( new node(15, "#0455cd", new THREE.Vector3(4,5,0), []) );
+    graphNodes.push( new node(15, "#0995cd", new THREE.Vector3(4,5,0), []) );
+}
+
+const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+
+function randomColor() {
+    return '#' + genRanHex(6);
+}
+
+function addDefaultNode() {
+    graphNodes.push( new node(5, randomColor(), new THREE.Vector3(0, 0 ,0), []) );
 }
 
 function addEdge(x, y) {
@@ -36,10 +46,24 @@ function addEdge(x, y) {
     ynode.connectedTo.push(xnode);
 }
 
-init();
+
+// init();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
+addDefaultNode();
 addEdge(0, 1);
 addEdge(0, 2);
 addEdge(2, 1);
+addEdge(3, 1);
+addEdge(4, 5);
+addEdge(7, 8);
+addEdge(8, 1);
 
 
 
@@ -79,7 +103,28 @@ graphNodes.forEach(nodeElem => {
 // function renderCylinderInbetweenTwoNodes()
 
 function spreadSpheres() {
-    
+    let cycle = 1;
+    const cubeHalfLength = 2;
+    for (let i = 0; i < graphNodes.length; i++) {
+        const sphere = scene.children[i];
+        let sub = i % 8;
+        if (sub == 0) cycle++;
+        let x, y, z;
+        
+        if (sub > 3) y = cubeHalfLength;
+        else y = -cubeHalfLength;
+
+        // 0 1 /2 /3 4 5 /6 /7
+        if (sub < 2 || (sub > 3 && sub < 6)) x = cubeHalfLength;
+        else x = -cubeHalfLength;
+
+        if (sub % 2 == 0) z = cubeHalfLength;
+        else z = -cubeHalfLength;
+        sphere.position.x = x * cycle;
+        sphere.position.y = y * cycle;
+        sphere.position.z = z * cycle;
+        console.log({x: x, y: y, z: z});
+    }
 }
 
 function createConnectionBetweenSpheres() {
@@ -141,7 +186,7 @@ var perpendicularRot = 0;
 // }
 
 function moveCamera() {
-    console.log("old", camera.position);
+    // console.log("old", camera.position);
 
     camera.position.x = cameraDistToCenter * Math.sin( parallelRot * (2 * Math.PI) ) * Math.cos(perpendicularRot * (2 * Math.PI));
     camera.position.y = cameraDistToCenter * Math.sin(perpendicularRot * (2 * Math.PI));
@@ -155,7 +200,7 @@ const cameraZoomSpeed = 0.15;
 
 
 function onkeydown(e) {
-    console.log(e.key);
+    // console.log(e.key);
     if (e.key == 'a') {
         parallelRot -= cameraSpeed;
         camera.rotateY(-cameraRotationSpeed);
@@ -187,5 +232,6 @@ function onkeydown(e) {
 document.addEventListener('keydown', onkeydown);
 
 moveCamera();
+spreadSpheres();
 createConnectionBetweenSpheres();
 animate();
